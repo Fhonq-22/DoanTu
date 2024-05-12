5408045fd52efc3547c4b8de4ce6e82b8c2e92df
Phòng <113459855+Fhonq-22@users.noreply.github.com>
2024-05-12 16:17:40 +0700

    Add files via upload

---
+++ DoanTatCa.js
@@ -0,0 +1,81 @@
+function loaiBoKyTuDacBiet(tu) {
+    return tu.replace(/[\n\r]/g, '');
+}
+
+function loaiBoTuHienTai() {
+    const index = dsTu.indexOf(tuHienTai);
+    if (index !== -1) {
+        dsTu.splice(index, 1);
+        console.log("Mảng dsTu sau khi loại bỏ '"+tuHienTai+"':", dsTu);
+    }
+}
+
+function chonTu() {
+    if (dsTu.length === 0) {
+        document.getElementById('message').textContent = 'Bạn đã đoán hết tất cả các từ!';
+        document.getElementById('inputDoan').disabled = true;
+        document.getElementById('btnDoan').disabled = true;
+        return;
+    }
+    const index = Math.floor(Math.random() * dsTu.length);
+    tuHienTai = dsTu[index];
+    tuHienTai = loaiBoKyTuDacBiet(tuHienTai);
+    const tuXaoTron = xaoTron(tuHienTai.replace(/\s/g, ''));
+    document.getElementById('word').textContent = tuXaoTron;
+    console.log('Từ hiện tại đã được chọn:', tuHienTai);
+}
+
+function xaoTron(word) {
+    let characters = word.split('');
+    for (let i = characters.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [characters[i], characters[j]] = [characters[j], characters[i]];
+    }
+    return characters.join('/');
+}
+
+function kiemTra() {
+    const doan = document.getElementById('inputDoan').value.trim().toLowerCase();
+    if (doan === tuHienTai) {
+        document.getElementById('message').textContent = 'Chính xác!';
+        document.getElementById('inputDoan').value = '';
+        loaiBoTuHienTai();
+        chonTu();
+    } else {
+        document.getElementById('message').textContent = 'Sai rồi, hãy thử lại!';
+    }
+}
+
+document.getElementById('btnDoan').addEventListener('click', kiemTra);
+document.getElementById('inputDoan').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        kiemTra();
+    }
+});
+
+let dsTu = [];
+let tuHienTai = '';
+
+fetch('https://raw.githubusercontent.com/Fhonq-22/DoanTu/main/Data.txt')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Mạng không ổn định');
+        }
+        return response.text();
+    })
+    .then(data => {
+        // Xử lý dữ liệu
+        dsTu = data.trim().split('\n').map(tu => tu.trim());
+        console.log('Mảng dsTu ban đầu:', dsTu);
+        chonTu();
+    })
+
+    .catch(error => {
+        console.error('Đã xảy ra sự cố khi tìm nạp dữ liệu:', error);
+    });
+
+export { loaiBoKyTuDacBiet };
+export { loaiBoTuHienTai };
+export { chonTu };
+export { xaoTron };
+export { kiemTra };
